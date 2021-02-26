006725dfb4e0604c46dc287eaa0da25cfd837dea
BBaoVanC <github@bbaovanc.com>
2021-02-26 10:30:42 -0600

    Change 4 tile color to CYAN

--- smines.c
+++ smines.c
@@ -28,7 +28,7 @@ int main() {
     init_pair(1, COLOR_WHITE, COLOR_BLUE);
     init_pair(2, COLOR_WHITE, COLOR_GREEN);
     init_pair(3, COLOR_WHITE, COLOR_RED);
-    init_pair(4, COLOR_WHITE, COLOR_BLUE);
+    init_pair(4, COLOR_WHITE, COLOR_CYAN);
     init_pair(5, COLOR_BLACK, COLOR_YELLOW);
     init_pair(6, COLOR_BLACK, COLOR_MAGENTA);
     init_pair(7, COLOR_WHITE, COLOR_BLACK);
