9a91d1d68a90bf11c548d72b58319f336cdbb466
BBaoVanC <github@bbaovanc.com>
2021-03-30 10:37:37 -0500

    Forgot to add draw_cursor to draw.h

--- draw.h
+++ draw.h
@@ -6,6 +6,7 @@
 #include <ncurses.h>
 #include "minefield.h"
 
+void draw_cursor(WINDOW *, Tile *, enum States);
 void draw_tile(WINDOW *, Tile *, bool, bool, bool);
 void draw_minefield(WINDOW *, Minefield *, bool, bool, enum States);
 void draw_scoreboard(WINDOW *, Minefield *, int, enum States);
