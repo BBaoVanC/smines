2e5058ceefb1a4b931c46d831bf641ff45302f8d
BBaoVanC <bbaovanc@bbaovanc.com>
2022-07-04 20:39:05 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use anyhow::Context;
+use std::{borrow::Borrow, io};
+
 use clap::Parser;
 
 use crossterm::{cursor, execute, terminal};
-
-use std::{borrow::Borrow, io, panic};
 
 use tui::{
     backend::Backend,
@@ -13,67 +14,84 @@ use tui::{
 };
 
 #[derive(Parser, Debug)]
-#[clap(
-    author,
-    version,
-    about,
-    long_about = "Simple minesweeper in the terminal."
-)]
+#[clap(author, version)]
+/// Simple minesweeper in the terminal.
 struct Args {
     #[clap(short, long, value_parser, default_value_t = 16)]
+    /// Total columns in the minefield
+    ///
+    /// This sets the amount of columns (lines stacked horizontally) in the
+    /// minefield.
     cols: u32,
     #[clap(short, long, value_parser, default_value_t = 16)]
+    /// Total rows in the minefield
+    ///
+    /// This sets the amount of rows (lines stacked vertically) in the
+    /// minefield.
     rows: u32,
     #[clap(short, long, value_parser, default_value_t = 40)]
+    /// Total mines in the minefield
+    ///
+    /// This sets the amount of mines that will be distributed in the minefield.
     mine_count: u32,
 
-    #[clap(short, long, value_parser, default_value_t = false)]
+    #[clap(short, long, value_parser)]
+    /// Should you be allowed to undo your last move?
+    ///
+    /// If true, then you can press `u` to undo your last move. Pressing `u`
+    /// again will "undo the undo" -- meaning that you'll be back to your
+    /// original move.
     allow_undo: bool,
 }
 
-fn setup_terminal() {
-    execute!(
-        io::stdout(),
-        terminal::EnterAlternateScreen,
-        terminal::Clear(terminal::ClearType::All),
-        cursor::Hide,
-    )
-    .unwrap();
-    terminal::enable_raw_mode().unwrap();
+struct Term {}
+impl Term {
+    fn new() -> Self {
+        execute!(
+            io::stdout(),
+            terminal::EnterAlternateScreen,
+            terminal::Clear(terminal::ClearType::All),
+            cursor::Hide,
+        )
+        .unwrap();
+        terminal::enable_raw_mode().unwrap();
+        Self {}
+    }
+}
+impl Drop for Term {
+    fn drop(&mut self) {
+        execute!(
+            io::stdout(),
+            cursor::MoveTo(0, 0),
+            terminal::Clear(terminal::ClearType::All),
+            terminal::LeaveAlternateScreen,
+            cursor::Show,
+        )
+        .unwrap();
+        terminal::disable_raw_mode().unwrap();
+    }
 }
 
-fn close_terminal() {
-    execute!(
-        io::stdout(),
-        cursor::MoveTo(0, 0),
-        terminal::Clear(terminal::ClearType::All),
-        terminal::LeaveAlternateScreen,
-        cursor::Show,
-    )
-    .unwrap();
-    terminal::disable_raw_mode().unwrap();
-}
-
-// Separate run() function to ensure that raw mode gets disabled even if there's an error
-fn main() {
-    let settings = better_panic::Settings::auto()
-        .most_recent_first(false)
-        .backtrace_first(false);
-    settings.clone().install();
-
-    panic::set_hook(Box::new(move |ctx| {
-        close_terminal();
-        settings.clone().create_panic_handler()(ctx);
-    }));
-
+// Separate run() function to ensure that raw mode gets disabled even if there's
+// an error
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let backend = CrosstermBackend::new(io::stdout());
-    let mut terminal = Terminal::new(backend).expect("Failed to create terminal");
+    {
+        let _term = Term::new();
 
-    setup_terminal();
-    terminal.draw(|f| ui(f, args)).expect("Failed to draw UI");
-    close_terminal();
+        let backend = CrosstermBackend::new(io::stdout());
+        let mut terminal = Terminal::new(backend).context("Failed to create terminal")?;
+
+        terminal
+            .draw(|f| ui(f, &args))
+            .context("Failed to draw UI")?;
+        std::thread::sleep(std::time::Duration::from_secs(3));
+    }
+
+    println!("{:?}", &args);
+
+    Ok(())
 }
 
 fn ui<B: Backend, C: Borrow<Args>>(f: &mut Frame<B>, config: C) {
