309b2639cc8e3f6d2b3c97b90bcadc64f6ca5c14
BBaoVanC <bbaovanc@bbaovanc.com>
2022-07-08 23:54:25 -0500

    Add demo.rs

---
+++ src/bin/demo.rs
@@ -0,0 +1,45 @@
+use smines::{
+    game::Game,
+    generate::TemplateMinefield,
+    minefield::{FieldDimension, Minefield},
+};
+
+// use std::io;
+// use crossterm::{cursor, execute, terminal};
+// struct Term;
+// impl Term {
+//     fn new() -> Self {
+//         execute!(
+//             io::stdout(),
+//             terminal::EnterAlternateScreen,
+//             terminal::Clear(terminal::ClearType::All),
+//             cursor::Hide,
+//         )
+//         .unwrap();
+//         terminal::enable_raw_mode().unwrap();
+//         Self {}
+//     }
+// }
+// impl Drop for Term {
+//     fn drop(&mut self) {
+//         execute!(
+//             io::stdout(),
+//             cursor::MoveTo(0, 0),
+//             terminal::Clear(terminal::ClearType::All),
+//             terminal::LeaveAlternateScreen,
+//             cursor::Show,
+//         )
+//         .unwrap();
+//         terminal::disable_raw_mode().unwrap();
+//     }
+// }
+
+fn main() {
+    // let _term = Term::new();
+
+    let game = Game::from_minefield(Minefield::from_template(
+        TemplateMinefield::new_randomly_spread(FieldDimension { x: 4, y: 4 }, 2),
+    ));
+
+    println!("{:#?}", game.display());
+}
